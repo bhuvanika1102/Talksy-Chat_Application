5196755e8eea708f13cb42f942bfb65a06210ee3
bhuvanikas <bhuvanikas@vitinfotech.com>
2026-01-28 18:17:04 +0530

    Integrated view/hide password functionality

--- talksy-frontend/src/components/TextInput.js
+++ talksy-frontend/src/components/TextInput.js
@@ -1,7 +1,41 @@
-import { TextField } from "@mui/material";
+import { useState } from "react";
+import {
+  TextField,
+  IconButton,
+  InputAdornment
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
-const TextInput = ({ label, ...props }) => {
-  return <TextField label={label} fullWidth margin="normal" {...props} />;
+const TextInput = ({ label, type = "text", ...props }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isPassword = type === "password";
+
+  return (
+    <TextField
+      fullWidth
+      margin="normal"
+      label={label}
+      type={isPassword && showPassword ? "text" : type}
+      InputProps={
+        isPassword
+          ? {
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => setShowPassword(!showPassword)}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              )
+            }
+          : undefined
+      }
+      {...props}
+    />
+  );
 };
 
 export default TextInput;
